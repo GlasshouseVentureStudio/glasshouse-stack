e4bade0cc9a8dd1f5d58f8ad722aaadfeb4840c3
Hieu Vi <hieu1871998@gmail.com>
2024-09-03 23:45:59 +0700

    chore(components): update build
    Signed-off-by: Hieu Vi <hieu1871998@gmail.com>

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.ts', 'src/**/index.{ts,tsx}', '!src/**/*.stories.{ts,tsx}', '!src/types'],
+	entry: ['src/index.ts', 'src/**/*.{ts,tsx}', '!src/**/*.stories.{ts,tsx}', '!src/types'],
 	format: ['cjs', 'esm'],
 	outDir: 'dist',
 	external: ['react', '@mantine/core', '@tanstack/react-query'],
-	minify: true,
-	sourcemap: true,
-	metafile: true,
 	loader: {
 		'.css': 'local-css',
 	},
